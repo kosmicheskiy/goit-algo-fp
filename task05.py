ebd21042a3e59068a35c8fd7a1ba004d89e488eb
kosmicheskiy <bezvoshchuk.v@gmail.com>
2024-05-27 10:50:50 +0300

    Update task05.py

--- task05.py
+++ task05.py
@@ -3,77 +3,85 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 class Node:
-    def __init__(self, key):
+    def __init__(self, key, color="skyblue"):
         self.left = None
         self.right = None
         self.val = key
-        self.id = str(uuid.uuid4())  # Унікальний ідентифікатор для кожного вузла
-        self.color = "#000000"  # Початковий колір - чорний
+        self.color = color
+        self.id = str(uuid.uuid4())
 
-def add_edges_dfs(graph, node, pos, x=0, y=0, layer=1, color_step=16):
+def add_edges(graph, node, pos, x=0, y=0, layer=1):
     if node is not None:
-        color = "#{:06x}".format(0x1296F0 - color_step * layer)  # Генерація кольору для вузла
-        graph.add_node(node.id, color=color, label=node.val)
+        graph.add_node(node.id, color=node.color, label=node.val)
         if node.left:
             graph.add_edge(node.id, node.left.id)
             l = x - 1 / 2 ** layer
             pos[node.left.id] = (l, y - 1)
-            add_edges_dfs(graph, node.left, pos, x=l, y=y - 1, layer=layer + 1, color_step=color_step)
+            l = add_edges(graph, node.left, pos, x=l, y=y - 1, layer=layer + 1)
         if node.right:
             graph.add_edge(node.id, node.right.id)
             r = x + 1 / 2 ** layer
             pos[node.right.id] = (r, y - 1)
-            add_edges_dfs(graph, node.right, pos, x=r, y=y - 1, layer=layer + 1, color_step=color_step)
+            r = add_edges(graph, node.right, pos, x=r, y=y - 1, layer=layer + 1)
+    return graph
 
-def add_edges_bfs(graph, root, pos, color_step=16):
-    queue = [(root, 0)]
+def draw_heap(heap_root):
+    heap = nx.DiGraph()
+    pos = {heap_root.id: (0, 0)}
+    heap = add_edges(heap, heap_root, pos)
+
+    colors = [node[1]['color'] for node in heap.nodes(data=True)]
+    labels = {node[0]: node[1]['label'] for node in heap.nodes(data=True)}
+
+    plt.figure(figsize=(8, 5))
+    nx.draw(heap, pos=pos, labels=labels, arrows=False, node_size=2500, node_color=colors)
+    plt.show()
+
+def depth_first_traversal(node, traversal_order):
+    if node:
+        traversal_order.append(node)
+        node.color = generate_color(len(traversal_order))
+        depth_first_traversal(node.left, traversal_order)
+        depth_first_traversal(node.right, traversal_order)
+
+def breadth_first_traversal(node, traversal_order):
+    queue = [node]
     while queue:
-        node, layer = queue.pop(0)
-        color = "#{:06x}".format(0x1296F0 - color_step * layer)
-        graph.add_node(node.id, color=color, label=node.val)
-        if node.left:
-            graph.add_edge(node.id, node.left.id)
-            pos[node.left.id] = (pos[node.id][0] - 1 / 2 ** (layer + 1), pos[node.id][1] - 1)
-            queue.append((node.left, layer + 1))
-        if node.right:
-            graph.add_edge(node.id, node.right.id)
-            pos[node.right.id] = (pos[node.id][0] + 1 / 2 ** (layer + 1), pos[node.id][1] - 1)
-            queue.append((node.right, layer + 1))
+        current_node = queue.pop(0)
+        if current_node:
+            traversal_order.append(current_node)
+            current_node.color = generate_color(len(traversal_order))
+            queue.append(current_node.left)
+            queue.append(current_node.right)
 
-def draw_tree_dfs(tree_root, color_step=16):
-    tree = nx.DiGraph()
-    pos = {tree_root.id: (0, 0)}
-    add_edges_dfs(tree, tree_root, pos, color_step=color_step)
+def generate_color(step):
+    # Generate a color based on the step in the traversal
+    color_value = 255 - int((step / 10) * 255)
+    color_hex = "#{:02X}{:02X}{:02X}".format(color_value, color_value, 255)
+    return color_hex
 
-    colors = [node[1]['color'] for node in tree.nodes(data=True)]
-    labels = {node[0]: node[1]['label'] for node in tree.nodes(data=True)}
+# Create a binary tree
+heap_root = Node(0)
+heap_root.left = Node(4)
+heap_root.right = Node(1)
+heap_root.left.left = Node(5)
+heap_root.left.right = Node(10)
+heap_root.right.left = Node(3)
 
-    plt.figure(figsize=(8, 5))
-    nx.draw(tree, pos=pos, labels=labels, arrows=False, node_size=2500, node_color=colors)
-    plt.show()
+# Depth-first traversal
+depth_first_order = []
+depth_first_traversal(heap_root, depth_first_order)
 
-def draw_tree_bfs(tree_root, color_step=64):
-    tree = nx.DiGraph()
-    pos = {tree_root.id: (0, 0)}
-    add_edges_bfs(tree, tree_root, pos, color_step=color_step)
+# Visualize depth-first traversal
+draw_heap(heap_root)
 
-    colors = [node[1]['color'] for node in tree.nodes(data=True)]
-    labels = {node[0]: node[1]['label'] for node in tree.nodes(data=True)}
+# Reset colors for breadth-first traversal
+for node in depth_first_order:
+    node.color = "skyblue"
 
-    plt.figure(figsize=(8, 5))
-    nx.draw(tree, pos=pos, labels=labels, arrows=False, node_size=2500, node_color=colors)
-    plt.show()
+# Breadth-first traversal
+breadth_first_order = []
+breadth_first_traversal(heap_root, breadth_first_order)
 
-# Створення дерева
-root = Node(0)
-root.left = Node(4)
-root.left.left = Node(5)
-root.left.right = Node(10)
-root.right = Node(1)
-root.right.left = Node(3)
-
-# Відображення дерева з обходом у глиб
-draw_tree_dfs(root)
-
-# Відображення дерева з обходом у ширину
-draw_tree_bfs(root)
+# Visualize breadth-first traversal
+draw_heap(heap_root)
