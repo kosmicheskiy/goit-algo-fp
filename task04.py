df9445ca0a1287af7ed3df7996ba821e31966de3
kosmicheskiy <bezvoshchuk.v@gmail.com>
2024-05-27 10:39:49 +0300

    Update task04.py

--- task04.py
+++ task04.py
@@ -1,41 +1,49 @@
-import heapq
+import uuid
 import networkx as nx
 import matplotlib.pyplot as plt
 
-class BinaryHeap:
-    def __init__(self):
-        self.heap = []
-    
-    def insert(self, value):
-        heapq.heappush(self.heap, value)
-    
-    def draw_heap(self):
-        heap_tree = nx.DiGraph()
-        pos = {}
+class Node:
+    def __init__(self, key, color="skyblue"):
+        self.left = None
+        self.right = None
+        self.val = key
+        self.color = color
+        self.id = str(uuid.uuid4())
 
-        for i, value in enumerate(self.heap):
-            node_id = str(i)
-            heap_tree.add_node(node_id, label=str(value))
-            pos[node_id] = (i, 0)
+def add_edges(graph, node, pos, x=0, y=0, layer=1):
+    if node is not None:
+        graph.add_node(node.id, color=node.color, label=node.val)
+        if node.left:
+            graph.add_edge(node.id, node.left.id)
+            l = x - 1 / 2 ** layer
+            pos[node.left.id] = (l, y - 1)
+            l = add_edges(graph, node.left, pos, x=l, y=y - 1, layer=layer + 1)
+        if node.right:
+            graph.add_edge(node.id, node.right.id)
+            r = x + 1 / 2 ** layer
+            pos[node.right.id] = (r, y - 1)
+            r = add_edges(graph, node.right, pos, x=r, y=y - 1, layer=layer + 1)
+    return graph
 
-            if i > 0:
-                parent_id = str((i - 1) // 2)
-                heap_tree.add_edge(parent_id, node_id)
+def draw_heap(heap_root):
+    heap = nx.DiGraph()
+    pos = {heap_root.id: (0, 0)}
+    heap = add_edges(heap, heap_root, pos)
 
-        labels = {node[0]: node[1]['label'] for node in heap_tree.nodes(data=True)}
+    colors = [node[1]['color'] for node in heap.nodes(data=True)]
+    labels = {node[0]: node[1]['label'] for node in heap.nodes(data=True)}
 
-        plt.figure(figsize=(8, 5))
-        nx.draw(heap_tree, pos=pos, labels=labels, arrows=False, node_size=2000, node_color='skyblue')
-        plt.show()
+    plt.figure(figsize=(8, 5))
+    nx.draw(heap, pos=pos, labels=labels, arrows=False, node_size=2500, node_color=colors)
+    plt.show()
 
-# Створення бінарної купи та вставка елементів
-heap = BinaryHeap()
-heap.insert(10)
-heap.insert(5)
-heap.insert(7)
-heap.insert(3)
-heap.insert(1)
-heap.insert(8)
+# Створення бінарної купи
+heap_root = Node(0)
+heap_root.left = Node(4)
+heap_root.right = Node(1)
+heap_root.left.left = Node(5)
+heap_root.left.right = Node(10)
+heap_root.right.left = Node(3)
 
-# Відображення бінарної купи
-heap.draw_heap()
+# Візуалізація бінарної купи
+draw_heap(heap_root)
